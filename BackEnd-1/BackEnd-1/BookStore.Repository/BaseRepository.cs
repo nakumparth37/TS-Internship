c2b9e1feeb687690875d47f2ce5710d6bd0d92df
Parth Nakum <75476666+nakumparth37@users.noreply.github.com>
2022-07-11 10:59:55 +0530

    BookStory Project

---
+++ BackEnd-1/BackEnd-1/BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.model.ViewModels;
+
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly BookStroeContext _context = new BookStroeContext();
+    }
+}
---
+++ BackEnd-1/BackEnd-1/BookStore.Repository/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.model.ViewModels;
+using BookStore.Models.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Repository
+{
+    public class UserRepository
+    {
+        BookStroeContext _context = new BookStroeContext();
+
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User Login(LoginModel model)
+        {
+            return _context.Users.FirstOrDefault(c => c.Email.Equals(model.Email.ToLower()) && c.Password.Equals(model.Password));
+        }
+
+        public User Register(RegisterModel model)
+        {
+            User user = new User()
+            {
+                Email = model.Email,
+                Password = model.Password,
+                Firstname = model.Firstname,
+                Lastname = model.Lastname,
+                Roleid = model.Roleid,
+            };
+            var entry = _context.Users.Add(user);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+    }
+}
